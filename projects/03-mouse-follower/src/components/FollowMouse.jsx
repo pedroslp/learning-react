766308054026601afd7776ae5c4a998207b173e7
Pedro Segundo López Porras <pslp130900@gmail.com>
2023-03-13 21:50:09 +0100

    Add FollowMouse component

---
+++ projects/03-mouse-follower/src/components/FollowMouse.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+
+export const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('effect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // Esto limpia el efecto cuando el componente se desmonta
+    // o cada vez que cambie la dependencia
+    // de esta forma nos aseguramos que empezamos de cero
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir cursor
+      </button>
+    </>
+  )
+}
--- projects/03-mouse-follower/src/main.jsx
+++ projects/03-mouse-follower/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+// El StrictMode unicamente funcion en desarrollo ( por eden, el useEffect se ejecuta 2 veces )
